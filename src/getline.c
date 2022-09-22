0f9ad5abedfca8d02015be1603888b374789f667
nm <nagamocha@gmail.com>
2022-09-22 16:13:30 +0300

    Fix errors and warnings

--- src/getline.c
+++ src/getline.c
@@ -1,9 +1,9 @@
-#include "getline.h"
+#include "../include/getline.h"
 #include <stdio.h>
 
 // my_getline: get line into s, return length
 int my_getline(char s[], register int lim) {
-    int c, i = 0;
+    int c = 0, i = 0;
     while (--lim > 0 && (c = getchar()) != EOF && c != '\n') {
         s[i++] = c;
     }
